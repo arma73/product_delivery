e2e0dd076513f8b19a6c5246b9ec1dbd7327c828
arma73 <arma.biz9805@gmail.com>
2021-04-10 20:42:07 +0400

    update: connect with db & get env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { RestaurantsModule } from "./restaurants/restaurants.module";
+import { getEnvFileName } from "./utils/getEnvFileName";
 
 @Module({
     "imports": [
+        ConfigModule.forRoot({
+            "isGlobal": true,
+            "envFilePath": getEnvFileName(),
+            "ignoreEnvFile": process.env.NODE_ENV === "production",
+        }),
+        TypeOrmModule.forRoot({
+            "type": "postgres",
+            "host": process.env.DB_HOST,
+            "port": +process.env.DB_PORT,
+            "username": process.env.DB_USER,
+            "password": process.env.DB_PASSWORD,
+            "database": process.env.DB_NAME,
+            "synchronize": true,
+            "logging": true,
+        }),
         GraphQLModule.forRoot({
             "autoSchemaFile": true,
         }),
