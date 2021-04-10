e9dd24004ceb90ca1525795b5c17c84d111c2b3d
arma73 <arma.biz9805@gmail.com>
2021-04-10 18:24:38 +0400

    add: init createRestaurant mutation

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,5 @@
-import { Resolver, Query, Args } from "@nestjs/graphql";
+import { Resolver, Query, Args, Mutation } from "@nestjs/graphql";
+import { CreateRestaurantDTO } from "./dtos/createRestaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
 
 @Resolver(of => Restaurant)
@@ -6,5 +7,12 @@ export class RestaurantResolver {
     @Query(returns => [Restaurant])
     restaurants(@Args("veganOnly") veganOnly: boolean): Restaurant[] {
         return [];
+    }
+
+    @Mutation(returns => Boolean)
+    createRestaurant(
+        @Args() createRestaurantDTO: CreateRestaurantDTO
+    ): boolean {
+        return true;
     }
 }
