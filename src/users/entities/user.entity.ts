8270dee270d5520703131b6622232eded335699b
arma73 <arma.biz9805@gmail.com>
2021-04-11 18:07:23 +0400

    update: with fields type

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,22 @@
 import { CoreEntity } from "../../common/entities/core.entity";
 import { Column, Entity } from "typeorm";
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
 
 type UserRole = "client" | "owner" | "delivery";
 
+@InputType({ "isAbstract": true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
+    @Field(type => String)
     @Column()
     email: string;
 
+    @Field(type => String)
     @Column()
     password: string;
 
+    @Field(type => String)
     @Column()
     role: UserRole;
 }
