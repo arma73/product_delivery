359b4b7badb28224820ec374043b97f0790cf746
arma73 <arma.biz9805@gmail.com>
2021-04-11 17:36:40 +0400

    update: with import users & common & temporarily remove restaurants

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { GraphQLModule } from "@nestjs/graphql";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import * as Joi from "joi";
-import { RestaurantsModule } from "./restaurants/restaurants.module";
 import { getEnvFileName } from "./utils/getEnvFileName";
-import { Restaurant } from "./restaurants/entities/restaurant.entity";
+import { UsersModule } from "./users/users.module";
+import { CommonModule } from "./common/common.module";
+import { User } from "./users/entities/user.entity";
 
 @Module({
     "imports": [
@@ -33,12 +34,13 @@ import { Restaurant } from "./restaurants/entities/restaurant.entity";
             "database": process.env.DB_NAME,
             "synchronize": process.env.NODE_ENV !== "production",
             "logging": process.env.NODE_ENV !== "production",
-            "entities": [Restaurant],
+            "entities": [User],
         }),
         GraphQLModule.forRoot({
             "autoSchemaFile": true,
         }),
-        RestaurantsModule,
+        UsersModule,
+        CommonModule,
     ],
     "controllers": [],
     "providers": [],
